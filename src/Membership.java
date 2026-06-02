56ea01c1faa784bc1fd40836aa53c8ff56ee8dd5
HridoyChandraDas <hridoychandradas.hd33@gmail.com>
2026-06-02 23:51:52 +0600

    Update Membership.java

--- src/Membership.java
+++ src/Membership.java
@@ -1,13 +1,14 @@
 public class Membership {
 
+    //private modifier is used to protect data from direct access
     private String packageName;
     private double fee;
-
+    //Constructor initializes membership information
     public Membership(String packageName, double fee) {
         this.packageName = packageName;
         this.fee = fee;
     }
-
+    
     public String getPackageName() {
         return packageName;
     }
